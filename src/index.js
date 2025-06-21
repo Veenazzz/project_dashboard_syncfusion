5521f50b9a66674ec835c95b085188f8c7f0449d
Veena Dwivedi <162319091+Veenazzz@users.noreply.github.com>
2025-06-21 16:07:45 +0530

    changes 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
-
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
